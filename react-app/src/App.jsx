1ac18487a30d7224156c126c44da585aabc75087
Dilshaan Grewal <dilshaan.grewal.001@gmail.com>
2023-08-02 14:23:13 +0100

    feat: bond card

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,8 +1,29 @@
 import React from "react";
-import { Pets } from "./components/pets/Pets";
+import { Bond } from "./components/bonds/Bond";
+
+const bondDataExample = {
+  cusip: 'NULL',
+  isin: "XS1988387210",
+  bond_currency: 'USD',
+  bond_maturity_date: '05/08/2021',
+  coupon_percent: 4.37,
+  unit_price: 90,
+  face_value: 1000,
+  issuer_name: 'BNPParibasIssu 4,37% Microsoft Corp (USD)',
+  type: 'CORP',
+  status: 'active',
+  // trade_type: 'buy',
+  // trade_currency: 'USD',
+  // quantity: 50,
+  // trade_settlement_date: '04/08/2021',
+  // trade_status: 'open',
+  // trade_date: '13/05/2021',
+  // book_name: 'trading_book_1',
+  // bond_holder: 'AZ Holdings Inc',
+};
 
 const App = () => {
-  return <Pets />;
+  return <Bond bondData={bondDataExample}/>;
 };
 
 export default App;
--- react-app/src/components/bonds/Bond.jsx
+++ react-app/src/components/bonds/Bond.jsx
@@ -1,6 +1,22 @@
 import React, { useState, useEffect } from "react";
 import Card from 'react-bootstrap/Card'
+import './Bond.css'
 
-export const Bond = () => {
-
+export const Bond = ({bondData}) => {
+    return (
+        <Card className='container'>
+            <Card.Body>
+                <Card.Title className="title"> ISIN: {bondData.isin} </Card.Title>
+                <Card.Text> CUSIP: {bondData.cusip} </Card.Text>
+                <Card.Text> Currency: {bondData.bond_currency} </Card.Text>
+                <Card.Text> Maturity: {bondData.bond_maturity_date} </Card.Text>
+                <Card.Text> Coupon Percentage: {bondData.coupon_percent} </Card.Text>
+                <Card.Text> Unit Price: {bondData.unit_price} </Card.Text>
+                <Card.Text> Face value: {bondData.face_value} </Card.Text>
+                <Card.Text> Issuer Name: {bondData.issuer_name} </Card.Text>
+                <Card.Text> Type: {bondData.type} </Card.Text>
+                <Card.Text> Status: {bondData.status} </Card.Text>
+            </Card.Body>
+        </Card>
+    )
 };
