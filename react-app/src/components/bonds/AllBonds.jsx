af39ae62568031878502d058d38e8ea9046dbf23
Dilshaan Grewal <dilshaan.grewal.001@gmail.com>
2023-08-04 09:10:33 +0100

    feat: hook up all bonds to backend

--- react-app/src/components/bonds/AllBonds.jsx
+++ react-app/src/components/bonds/AllBonds.jsx
@@ -7,17 +7,16 @@ import { Row } from "react-bootstrap";
 export const AllBonds = () => {
     const [bonds, setBonds] = useState([]);
 
-    // TODO: uncomment when service is available
-    // useEffect(() => {
-    //     getAllBonds()
-    //         .then(({ data }) => {
-    //             setBonds(data);
-    //         });
-    // }, []);
+    useEffect(() => {
+        getAllBonds()
+            .then(({ data }) => {
+                setBonds(data);
+            });
+    }, []);
 
     // Temporary data
-    const temp_data = [bondDataExample, bondDataExample];
-    useEffect(() => { setBonds(temp_data); }, []);
+    // const temp_data = [bondDataExample, bondDataExample];
+    // useEffect(() => { setBonds(temp_data); }, []);
 
     return (
         <>
@@ -30,16 +29,17 @@ export const AllBonds = () => {
     )
 };
 
-const bondDataExample = {
-    cusip: 'NULL',
-    isin: "XS1988387210",
-    bond_currency: 'USD',
-    bond_maturity_date: '05/08/2021',
-    coupon_percent: 4.37,
-    unit_price: 90,
-    face_value: 1000,
-    issuer_name: 'BNPParibasIssu 4,37% Microsoft Corp (USD)',
-    type: 'CORP',
-    status: 'active',
-    bond_holder: 'AZ Holdings Inc',
-};
+
+// const bondDataExample = {
+//     cusip: 'NULL',
+//     isin: "XS1988387210",
+//     bond_currency: 'USD',
+//     bond_maturity_date: '05/08/2021',
+//     coupon_percent: 4.37,
+//     unit_price: 90,
+//     face_value: 1000,
+//     issuer_name: 'BNPParibasIssu 4,37% Microsoft Corp (USD)',
+//     type: 'CORP',
+//     status: 'active',
+//     bond_holder: 'AZ Holdings Inc',
+// };
