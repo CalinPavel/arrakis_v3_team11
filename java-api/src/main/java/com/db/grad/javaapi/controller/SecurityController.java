6ca4c7c2669b831a59ebe5b08477336f83c32d7a
CalinPavel <calinpav21@gmail.com>
2023-08-08 12:47:56 +0300

    Security token

--- java-api/src/main/java/com/db/grad/javaapi/controller/SecurityController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/SecurityController.java
@@ -1,11 +1,17 @@
 package com.db.grad.javaapi.controller;
 
 import com.db.grad.javaapi.dto.Bond;
+import com.db.grad.javaapi.model.AppUser;
 import com.db.grad.javaapi.model.Security;
 import com.db.grad.javaapi.service.SecurityHandler;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Base64;
 import java.util.List;
 
 @RestController
@@ -15,8 +21,12 @@ public class SecurityController {
 
     private SecurityHandler securityService;
 
+    private final FirebaseAuth firebaseAuth;
+
     @Autowired
-    public SecurityController(SecurityHandler sh){securityService = sh;}
+    public SecurityController(SecurityHandler sh, FirebaseAuth firebaseAuth){securityService = sh;
+        this.firebaseAuth = firebaseAuth;
+    }
 
     @GetMapping("/securities/{issuerName}")
     public List<Security> getSecurityByIssuerName(@PathVariable(value = "issuerName") String issuerName){
@@ -35,8 +45,24 @@ public class SecurityController {
     }
 
     @GetMapping("/securities/get5daysbonds/{dateRequest}")
-    public List<Bond> getSecurityBy5DaysBondsDate(@PathVariable(value = "dateRequest") String dateRequest){
-        return securityService.getSecurityBy5DaysBondsDate(dateRequest);
+    public ResponseEntity<?> getSecurityBy5DaysBondsDate(@PathVariable(value = "dateRequest") String dateRequest , @RequestHeader("Authorization") String authorizationHeader){
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Basic ")) {
+            String base64Credentials = authorizationHeader.substring("Basic ".length()).trim();
+            String credentials = new String(Base64.getDecoder().decode(base64Credentials));
+            String[] usernamePassword = credentials.split(":");
+            String email = usernamePassword[0];
+            String password = usernamePassword[1];
+
+            try {
+                FirebaseToken decodedToken = firebaseAuth.verifyIdToken(password);
+                System.out.println("Valid!");
+            } catch (FirebaseAuthException e) {
+                return ResponseEntity.badRequest().body("Invalid token: " + e.getMessage());
+            }
+        }
+        List<Bond> bonds = securityService.getSecurityBy5DaysBondsDate(dateRequest);
+        return ResponseEntity.ok(bonds);
     }
 
 }
