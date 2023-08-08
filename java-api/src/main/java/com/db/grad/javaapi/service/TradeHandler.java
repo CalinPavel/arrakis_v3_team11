b700aa11da805fa2227fd01d94933e096dd3b5c9
CalinPavel <calinpav21@gmail.com>
2023-08-08 11:36:47 +0300

    Trades Controller (filter by date)

--- java-api/src/main/java/com/db/grad/javaapi/service/TradeHandler.java
+++ java-api/src/main/java/com/db/grad/javaapi/service/TradeHandler.java
@@ -1,5 +1,6 @@
 package com.db.grad.javaapi.service;
 
+import com.db.grad.javaapi.dto.Bond;
 import com.db.grad.javaapi.model.Book;
 import com.db.grad.javaapi.model.Trade;
 import com.db.grad.javaapi.repository.BookRepository;
@@ -7,6 +8,12 @@ import com.db.grad.javaapi.repository.TradeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -37,6 +44,46 @@ public class TradeHandler implements ITradeService{
     @Override
     public void deleteTrade(Integer tradeId) {
         tradeRepository.deleteById(tradeId);
+    }
+
+    public List<Trade> getTradesByDate(String dateRequest){
+        List<Trade> result = new ArrayList<>();
+        List<Trade> all = tradeRepository.findAll();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate check = LocalDate.parse(dateRequest, formatter);
+
+        LocalDate fiveDaysBeforeCurrentDate = check.minusDays(5);
+        LocalDate fiveDaysAfterCurrentDate = check.plusDays(5);
+
+        for (Trade itr : all) {
+            String convertedDate = convertToYYYYMMDD(itr.getTradeDate());
+
+            if(convertedDate == null)
+                return null;
+
+            LocalDate checkFromArray = LocalDate.parse(convertedDate, formatter);
+
+            if (checkFromArray.isAfter(fiveDaysBeforeCurrentDate) && checkFromArray.isBefore(fiveDaysAfterCurrentDate)) {
+                System.out.println("Input date is within ±5 days from the desired date.");
+                result.add(itr);
+            }
+        }
+        return result;
+    }
+
+
+    public static String convertToYYYYMMDD(String inputDate) {
+        SimpleDateFormat inputFormat = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        try {
+            Date date = inputFormat.parse(inputDate);
+            return outputFormat.format(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
